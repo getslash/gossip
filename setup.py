a9373e33654cd5a0d1d222455c8a1f1a906e0a0b
Rotem Yaari <vmalloc@gmail.com>
2015-07-29 10:38:13 +0300

    Cosmetics

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 import os
 import sys
-from setuptools import setup, find_packages
+
+from setuptools import find_packages, setup
 
 with open(os.path.join(os.path.dirname(__file__), "gossip", "__version__.py")) as version_file:
-    exec(version_file.read()) # pylint: disable=W0122
+    exec(version_file.read())  # pylint: disable=W0122
 
 _INSTALL_REQUIERS = []
 
 setup(name="gossip",
-      classifiers = [
+      classifiers=[
           "Programming Language :: Python :: 2.6",
           "Programming Language :: Python :: 2.7",
           "Programming Language :: Python :: 3.3",
           "Programming Language :: Python :: 3.4",
-          ],
+      ],
       description="Signaling and hooking library",
       license="BSD3",
       author="Rotem Yaari",
       author_email="vmalloc@gmail.com",
-      version=__version__, # pylint: disable=E0602
+      version=__version__,  # pylint: disable=E0602
       packages=find_packages(exclude=["tests"]),
 
       url="https://github.com/vmalloc/gossip",
