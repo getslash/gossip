7094d0ac7dfb6b4703e251fe839f5e6184ed9f64
Ayala Shachar <shachar.ayala@gmail.com>
2017-03-05 10:48:01 +0200

    Save unmet dependencies on exception object (CannotResolveDependencies)

--- gossip/hooks.py
+++ gossip/hooks.py
@@ -172,7 +172,8 @@ class Hook(object):
     def trigger(self, kwargs, tags=None):
         if self._unmet_deps:
             deps_str = ', '.join([str(dep) for dep in self._unmet_deps])
-            raise CannotResolveDependencies('Hook {0!r} has unmet dependencies: {1}'.format(self, deps_str))
+            raise CannotResolveDependencies('Hook {0!r} has unmet dependencies: {1}'.format(self, deps_str),
+                                            unmet_deps=self._unmet_deps)
         if self.full_name in _muted_stack[-1]:
             _logger.debug("Hook {0!r} muted, skipping trigger", self)
             return
