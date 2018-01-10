b3614404246ff821b745d47cf6dbcdcf788a803b
Ayala Shachar <shachar.ayala@gmail.com>
2018-01-10 17:32:57 +0200

    Switch to PBR (fix #30)

--- setup.py
+++ setup.py
@@ -1,32 +1,8 @@
-import os
+#!/usr/bin/env python
+from setuptools import setup
 
-from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), "gossip", "__version__.py")) as version_file:
-    exec(version_file.read())  # pylint: disable=W0122
-
-_INSTALL_REQUIERS = [
-    'logbook>=0.12.0',
-    'vintage',
-    'sentinels',
-]
-
-setup(name="gossip",
-      classifiers=[
-          "Programming Language :: Python :: 2.7",
-          "Programming Language :: Python :: 3.5",
-          "Programming Language :: Python :: 3.6",
-      ],
-      description="Signaling and hooking library",
-      license="BSD3",
-      author="Rotem Yaari",
-      author_email="vmalloc@gmail.com",
-      version=__version__,  # pylint: disable=E0602
-      packages=find_packages(exclude=["tests"]),
-
-      url="https://github.com/getslash/gossip",
-
-      install_requires=_INSTALL_REQUIERS,
-      scripts=[],
-      namespace_packages=[]
-     )
+setup(
+    setup_requires=['pbr>=3.0', 'setuptools>=17.1'],
+    pbr=True,
+)
